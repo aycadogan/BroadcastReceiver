bc2b1b3d161d32489c7d396608b95dd587824a55
Ayca Dogan <aycadgan@hotmail.com>
2019-04-10 15:05:16 +0200

    power connected method

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SystemBroadcastReceiver mySystemReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button btnEnableReceiver = findViewById(R.id.activity_main__btn__enable_reciever);
+        btnEnableReceiver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mySystemReceiver = new SystemBroadcastReceiver();
+                registerReceiver(mySystemReceiver,
+                        new IntentFilter(Intent.ACTION_BATTERY_LOW));
+            }
+        });
+
+        final ConstraintLayout clRoot = findViewById(R.id.);
+        clRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent customIntent = new Intent();
+                customIntent.setAction("activity_main_cl__root");
+                sendBroadcast(customIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mySystemReceiver);
     }
 }
+
+
--- app/src/main/java/com/example/myapplication/SystemBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/SystemBroadcastReceiver.java
@@ -9,6 +9,17 @@ public class SystemBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        String message="";
+
+        switch(intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED:
+                message = "Power connected";
+                break;
+            case Intent.ACTION_BATTERY_LOW:
+                message = "Low Battery";
+                break;
+            default:
+        }
         Toast.makeText(context, "Power connected", Toast.LENGTH_LONG).show();
 
     }
